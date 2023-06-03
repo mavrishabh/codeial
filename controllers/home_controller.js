b79a0b9232a262b1dd51f679afa7f04a907d061a
Rishabh Singh <rishabhsingh812002@gmail.com>
2023-06-03 21:12:46 +0530

    Posts and User name were mentioned on the Home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,22 @@
+const Post = require('../models/post');
+
 module.exports.home = function(req, res){
-    console.log(req.cookies);
+    // console.log(req.cookies);
     // res.cookie('user_id', 25); 
     // return res.end('<h1> Express is up for codeial</h1>');
-    return res.render('home', {
-        title: "Home"
+    
+    // Post.find({}).then(posts =>{
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+
+    // Populate the user for each post
+    Post.find({}).populate('user').exec().then(posts =>{
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
     });
 }

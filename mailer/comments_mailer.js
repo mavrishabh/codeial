fcf4f29b0bfc86de9182581dd6cd9884d52c113a
Rishabh Singh <rishabhsingh812002@gmail.com>
2023-06-15 06:20:36 +0530

    Added mailer to the project

--- mailer/comments_mailer.js
+++ mailer/comments_mailer.js
@@ -3,13 +3,13 @@ const nodeMailer = require('../config/nodemailer');
 
 // This is another way of exporting a method
 exports.newComment = (comment) => {
-    console.log('inside newComment mailer');
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
 
     nodeMailer.transporter.sendMail({
         from: 'rishabhsingh.maverick@gmail.com',
         to: comment.user.email,
         subject: "New Comment Published!",
-        html: '<he> Yup, your comment is now published </h1>'
+        html: htmlString
     }, (err, info) => {
         if(err){
             console.log('Error in sending mail', err);
